067b0711d8ad41f511862165176e2c2dc30cfd59
ting-prado <ethel.prado1998@gmail.com>
2022-05-22 06:30:53 +0800

    changed port to 3000

--- src/server/index.js
+++ src/server/index.js
@@ -72,7 +72,7 @@ Promise.resolve(
       }
     });
     app.use(express.static(path.join(__dirname, 'dist')));
-    app.set('port', process.env.PORT || 8080);
+    app.set('port', process.env.PORT || 3000);
 
     var server = app.listen(app.get('port'), function () {
       console.log('listening on port ', server.address().port);
