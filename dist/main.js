e1b728403a8d533d5ef416387c0263d3fb5f61c0
ting-prado <ethel.prado1998@gmail.com>
2022-05-22 08:18:08 +0800

    restored dist

---
+++ dist/main.js
@@ -0,0 +1,58 @@
+/* TODO: Change toolbox XML ID if necessary. Can export toolbox XML from Workspace Factory. */
+const toolbox = document.getElementById('toolbox');
+
+const options = {
+  toolbox: toolbox,
+  collapse: true,
+  comments: true,
+  disable: true,
+  maxBlocks: Infinity,
+  trashcan: true,
+  horizontalLayout: false,
+  toolboxPosition: 'start',
+  css: true,
+  media: 'https://blockly-demo.appspot.com/static/media/',
+  rtl: false,
+  scrollbars: true,
+  sounds: true,
+  oneBasedIndex: true,
+  grid: {
+    spacing: 20,
+    length: 1,
+    colour: '#888',
+    snap: false,
+  },
+  zoom: {
+    controls: true,
+    wheel: true,
+    startScale: 1,
+    maxScale: 5,
+    minScale: 0.3,
+    scaleSpeed: 1.2,
+  },
+};
+
+/* Inject your workspace */
+var demoWorkspace = Blockly.inject('blocklyDiv', options);
+
+const showCode = () => {
+  const codeOutput = document.querySelector('#codeOutput');
+  Blockly.Verilog.INFINITE_LOOP_TRAP = null;
+  var code = Blockly.Verilog.workspaceToCode(demoWorkspace);
+  codeOutput.textContent = code;
+};
+
+demoWorkspace.addChangeListener(showCode);
+
+const cpyBtn = document.querySelector('.copy');
+
+function copyCode() {
+  var r = document.createRange();
+  r.selectNode(document.getElementById('codeOutput'));
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(r);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
+}
+
+cpyBtn.addEventListener('click', copyCode);
