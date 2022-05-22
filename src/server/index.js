bc0cd90741187233b21a4a72d7be82b903ccb0ef
ting-prado <ethel.prado1998@gmail.com>
2022-05-22 08:34:16 +0800

    set to localhost

--- src/server/index.js
+++ src/server/index.js
@@ -73,10 +73,11 @@ Promise.resolve(
       }
     });
     app.use(express.static(path.join(__dirname, '../../dist')));
-    app.set('port', process.env.PORT || 8080);
+    app.listen(8080, 'localhost');
+    // app.set('port', process.env.PORT || 8080);
 
-    var server = app.listen(app.get('port'), function () {
-      console.log('listening on port ', server.address().port);
-    });
+    // var server = app.listen(app.get('port'), function () {
+    //   console.log('listening on port ', server.address().port);
+    // });
   })()
 );
